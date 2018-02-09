8077c2c8f9fc30994f903044722f1db994178bb1
Yoscroll <ianhenry.c808@gmail.com>
2018-02-09 12:54:44 -1000

    Working

--- js/pizza.js
+++ js/pizza.js
@@ -4,6 +4,72 @@
 * Each object will have two nested objects ex.(pizza & toppings)
 * Keys with more than one value should be an array of strings ex. Veggies
 */
+var order11821={
+	firstname:"James",
+	lastname:"Franko",
+	City:"Seattle",
+	State:"WA",
+	PizzaCrust:"Thin",
+	PizzaSauce:"robust marinara",
+	PizzaCheese:"Regular"
+var toppings = [
+var Veggies = [
+"Olives", 
+"Green Peppers"
+];
+
+var Meats = [
+"Pepperoni",
+"Ham",
+"Sausage"
+];
+
+]
+var sideItems = [
+"Breadsticks", 
+"BBQ Wings 6 Piece"
+]
+    Sauces:"Ranch",
+    SpecialInstructions: "Please Cut Into Squares"
+    PaymentType: "Cash"
+    id:112
+};
+//1st Order Stops Here!
+
+var order11372={
+	firstname:"Justin",
+	lastname:"Bieber",
+	CityAddress:"1801 W Rodeo Dr"
+	City:"Beverly Hills",
+	State:"CA",
+	PizzaCrust:"stuffed crust",
+	PizzaSauce:"creamy garlic alfredo",
+	PizzaCheese:"double"
+var toppings = [
+var Veggies = [
+"onion", 
+"anchovies",
+"mushrooms"
+];
+
+var Meats = [
+"chicken",
+"bacon"
+];
+
+]
+    SideItems:"double fudge brownie dessert"
+
+var Sauces = [
+"BBQ", 
+"garlic sauce"
+]  
+
+    SpecialInstructions: "call 815 at the gate"
+    PaymentType: "credit card"
+    id:382
+};
+//2nd Order Stops Here!
 
 
 /*
@@ -11,6 +77,69 @@
 * currentOrders should be single object containing both orders from above
 */
 
+var order11821={
+	firstname:"James",
+	lastname:"Franko",
+	City:"Seattle",
+	State:"WA",
+	PizzaCrust:"Thin",
+	PizzaSauce:"robust marinara",
+	PizzaCheese:"Regular"
+var toppings = [
+var Veggies = [
+"Olives", 
+"Green Peppers"
+];
+
+var Meats = [
+"Pepperoni",
+"Ham",
+"Sausage"
+];
+
+]
+var sideItems = [
+"Breadsticks", 
+"BBQ Wings 6 Piece"
+]
+    Sauces:"Ranch",
+    SpecialInstructions: "Please Cut Into Squares"
+    PaymentType: "Cash"
+    id:112
+}
+var order11372={
+	firstname:"Justin",
+	lastname:"Bieber",
+	CityAddress:"1801 W Rodeo Dr"
+	City:"Beverly Hills",
+	State:"CA",
+	PizzaCrust:"stuffed crust",
+	PizzaSauce:"creamy garlic alfredo",
+	PizzaCheese:"double"
+var toppings = [
+var Veggies = [
+"onion", 
+"anchovies",
+"mushrooms"
+];
+
+var Meats = [
+"chicken",
+"bacon"
+];
+
+]
+    SideItems:"double fudge brownie dessert"
+
+var Sauces = [
+"BBQ", 
+"garlic sauce"
+]  
+
+    SpecialInstructions: "call 815 at the gate"
+    PaymentType: "credit card"
+    id:382
+};
 /*
 * Make the changes to the currentOrders object based on the ReadMe
 * Run your file with node pizza.js
